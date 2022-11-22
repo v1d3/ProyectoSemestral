f2b05fc5422d5bd11db125026444eb1f2e8a8905
v1d3 <ccristobalc132@gmail.com>
2022-11-22 18:10:36 -0300

    Merging

--- Main/src/main/Ventana.java
+++ Main/src/main/Ventana.java
@@ -1,17 +1,20 @@
-
 package main;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import javax.swing.JFrame;
 
-
 public class Ventana extends JFrame {
-    public Dimension d = new Dimension( 1250 , 800);
-    public Ventana(){
+
+    public Dimension d = new Dimension(1250, 800);
+
+    public Ventana() {
+        
         this.setLayout(new BorderLayout());
-        this.setSize(d);
-        this.setVisible(true);
-        setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE); //Activa cierre x
+        this.setSize(d);                                                           //Establece el Tamaño de la Ventana
+        this.setVisible(true);                                                  //Visibiliza la ventana
+        setLocationRelativeTo(null);                                    //Centra la aparicion de la Ventana
+        
     }
 }
