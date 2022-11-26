8f6eed96372891a8fcb91fb36985cf07ed7551de
v1d3 <ccristobalc132@gmail.com>
2022-11-26 19:14:29 -0300

    Adicion de poligono con movimiento

--- Main/src/main/PanelPrincipal.java
+++ Main/src/main/PanelPrincipal.java
@@ -2,9 +2,19 @@ package main;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Polygon;
+import java.awt.Toolkit;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import javax.swing.JPanel;
 
-public class PanelPrincipal extends JPanel {
+public class PanelPrincipal extends JPanel implements KeyListener {
+
+    private boolean a, s, w, d;
+    float x = 525f; //Coordenada x
+    float y = 400f;//Coordenada y
+    float angle = 0f; //Angulo
+    Polygon p;//Figura
 
     public PanelPrincipal() {
         this.setBackground(Color.blue);
@@ -12,13 +22,103 @@ public class PanelPrincipal extends JPanel {
 
     public void paint(Graphics g) {
         super.paint(g);
-        
+        g.setColor(Color.black);
+        g.fillRect(280, 30, 750, 700);
         g.setColor(Color.gray);
         g.fillRect(190, 500, 70, 150); // vertical
         g.fillRect(30, 580, 150, 70);
         g.setColor(Color.red);
         g.fillRect(1060, 580, 70, 70);// izq
         g.fillRect(1170, 580, 70, 70);//der
+        // input
+        if (w) {
+            x += 0.4f * Math.cos(Math.toRadians(angle));
+            y += 0.4f * Math.sin(Math.toRadians(angle));
+        }
+        if (s) {
+            x -= 0.1f * Math.cos(Math.toRadians(angle));
+            y -= 0.1f * Math.sin(Math.toRadians(angle));
+        }
+        if (a) {
+            angle -= 0.1f;
+        }
+        if (d) {
+            angle += 0.1f;
+        }
+
+        // update
+        update_auto();
+
+        // draw
+        g.setColor(Color.red);
+        g.fillPolygon(p);   //paint del polygon del auto
+
+        Toolkit.getDefaultToolkit().sync(); //para la inestabilidad del framerate
+        g.dispose();
+        repaint();
     }
 
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_W) {
+            w = true;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_A) {
+            a = true;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_S) {
+            s = true;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_D) {
+            d = true;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_W) {
+            w = false;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_A) {
+            a = false;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_S) {
+            s = false;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_D) {
+            d = false;
+        }
+    }
+
+    public void update_auto() {
+        float cos = (float) Math.cos(Math.toRadians(angle));
+        float sin = (float) Math.sin(Math.toRadians(angle));
+
+        p = new Polygon();
+
+        //para los sigueintes puntos se considera que el auto está en horizontal (angulo 0)
+        //punto adelante-izquierda del auto
+        float px = 100f;
+        float py = -20f;
+        p.addPoint((int) (x + px * cos - py * sin), (int) (y + px * sin + py * cos));
+
+        //punto atras-izquierda del auto
+        px = -60f;
+        py = -60f;
+        p.addPoint((int) (x + px * cos - py * sin), (int) (y + px * sin + py * cos));
+
+        //punto atras-derecha del auto
+        px = -60f;
+        py = 60f;
+        p.addPoint((int) (x + px * cos - py * sin), (int) (y + px * sin + py * cos));
+
+        //punto adelante-derecha del auto
+        px = 100f;
+        py = 20f;
+        p.addPoint((int) (x + px * cos - py * sin), (int) (y + px * sin + py * cos));
+    }
 }
--- Main/src/main/Ventana.java
+++ Main/src/main/Ventana.java
@@ -3,35 +3,30 @@ package main;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 
 public class Ventana extends JFrame {
 
-    private PanelPrincipal dp;
-    private Dimension d = new Dimension(1300, 800);
+    private PanelPrincipal panel;
+    private Dimension WindowSize = new Dimension(1300, 800);
+
     public Ventana() {
         this.setLayout(new BorderLayout());
-        dp = new PanelPrincipal();
-        dp.setLayout(null);
 
+        panel = new PanelPrincipal();
+        panel.setLayout(null);
+        this.add(panel);
+        this.addKeyListener(panel);
 
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setSize(d);
-        dp.setLayout(null);
+        this.add(panel);
 
-        setLocationRelativeTo(null);
-        this.add(dp);
-        this.setVisible(true);
-/////////////////////////////////////////////////////////////
-
-        /*Label l = new Label();
-        l.setText("This is a demonstration");
-        dp.add(l);*/
-
-        Teclado k = new Teclado();
-      this.addKeyListener(k);
-/////////////////////////////////////////////////////////////
-
+        InitWindow();
     }
 
+    private void InitWindow() {
+        this.setTitle("CarRace");
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setSize(WindowSize);
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
 }
